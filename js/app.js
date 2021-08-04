69c0b47b9e676b5dbbf9cee991ad9a343cbf8bd3
shiwani jha <shiwanikumari835@gmail.com>
2021-08-04 23:26:32 +0530

    initial commmit

---
+++ js/app.js
@@ -0,0 +1,83 @@
+showNotes();
+
+// function to store user input in local storage 
+
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null || notes == "") {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+
+  showNotes();
+});
+
+// function to add note and show it in card
+
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `<div class=" noteCard card mx-3 my-3" style="width: 18rem">
+             <div class="card-body">
+               <h5 class="card-title">Note ${index + 1}</h5>
+               <p class="card-text">
+                  ${element}
+               </p>
+               <button id=${index} onclick="deleteNote(this.id)" class="btn btn-primary">Delete note</button>
+              </div>
+            </div>`;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm = `nothing to show!use "add a note" section to add notes`;
+  }
+}
+
+// function to delete a note 
+
+function deleteNote(index) {
+ 
+  let notes = localStorage.getItem("notes");
+
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.splice(index,1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes()
+}
+
+// function to search notes 
+ let search=document.getElementById("searchTxt")
+  search.addEventListener("input",function(){
+    let inputVal=search.value.toLowerCase()
+   
+    let noteCards=document.getElementsByClassName("noteCard");
+    Array.from(noteCards).forEach(function(element){
+      let cardTxt=element.getElementsByTagName("p")[0].innerText;
+      if(cardTxt.includes(inputVal)){
+          element.style.display="block";
+        
+      }
+      else{
+        element.style.display="none";
+      }
+    })
+  })
